652f882158abb1c601c9bbc1165897e04ff7c94e
wgolanka <wiktoria.golanka@gmail.com>
2017-12-27 15:21:55 +0100

    Handles new socket connections.

---
+++ src/com/company/ServerMain.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(10000);
+
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                Runnable connectionHandler = new ConnectionHandler(clientSocket);
+                new Thread(connectionHandler).start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
