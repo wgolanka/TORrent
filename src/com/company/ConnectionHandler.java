c7856085c58c45a10d22fe013574313c189a8dda
wgolanka <wiktoria.golanka@gmail.com>
2017-12-27 15:21:25 +0100

    Handles new socket connections.

--- src/com/company/ConnectionHandler.java
+++ src/com/company/ConnectionHandler.java
@@ -3,36 +3,38 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ConnectionHandler implements Runnable {
+    private Socket clientSocket;
 
     public ConnectionHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
     }
 
     @Override
     public void run() {
+        BufferedReader in;
+        System.out.println("ConnectionHandler: run");
 
-        Server server = new Server(10000);
-        try (ServerSocket serverSocket =
-                     new ServerSocket(server.portNumber)) {
+        try {
+            System.out.println("Client connected: " + clientSocket.getRemoteSocketAddress().toString());
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String nextLine;
+            System.out.println("connectionHandler before");
+            while (true) {
+                if ((nextLine = in.readLine()) != null) {
+                    System.out.println(nextLine);
+                }
 
-            Socket connectionSocket = serverSocket.accept();
-            System.out.println("Client accepted: " + connectionSocket.getRemoteSocketAddress().toString());
+            }
 
-            BufferedReader inFromClient =
-                    new BufferedReader(
-                            new InputStreamReader(
-                                    connectionSocket.getInputStream())
-                    );
-
-            server.checkClientCommand(inFromClient.readLine(), serverSocket);
+//            System.out.println("connectionhandler after");
 
         } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + server.portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
+
     }
+
 }
